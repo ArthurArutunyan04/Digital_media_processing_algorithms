bccd8b02c0198dbdb0df195e34e7b75cc7ecb4f8
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-09-27 01:15:23 +0300

    Lab3 add task 2

---
+++ Lab3/task_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def gaussian_matrix(size, sigma=1.0, normalize=False):
+    center = size // 2
+    matrix = np.zeros((size, size), dtype=float)
+
+    for x in range(size):
+        for y in range(size):
+            dx = x - center
+            dy = y - center
+            matrix[x, y] = (1 / (2 * np.pi * sigma**2)) * np.exp(-(dx**2 + dy**2) / (2 * sigma**2))
+
+    if normalize:
+        matrix /= matrix.sum()
+
+    return matrix
+
+
+sizes = [3, 5, 7]
+sigma = 1.0
+
+np.set_printoptions(precision=4, suppress=True)
+
+for size in sizes:
+    print(f"\nНормированная матрица Гаусса {size}x{size} (sigma={sigma}):")
+    matrix = gaussian_matrix(size, sigma, normalize=True)
+    print(matrix)
+    print("Сумма элементов:", matrix.sum())
