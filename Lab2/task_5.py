38fe8ce80a7276a1b394e4e912dfce542f0cf279
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-10-03 22:22:32 +0300

    fix task_5(remove findContours)

--- Lab2/task_5.py
+++ Lab2/task_5.py
@@ -12,31 +12,40 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 131, 58])
-    upper_red = np.array([255, 255, 255])
+    lower_red1 = np.array([0, 120, 70])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([170, 120, 70])
+    upper_red2 = np.array([180, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
 
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((7, 7), np.uint8)
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
     moments = cv2.moments(closing)
     area = moments['m00'] if moments['m00'] > 0 else 0
+
     if area > 100:
         center_x = int(moments['m10'] / moments['m00'])
         center_y = int(moments['m01'] / moments['m00'])
         cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
-    else:
-        center_x, center_y = 0, 0
-        print("Объект не найден")
 
-    contours, _ = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        y_coords, x_coords = np.where(closing > 0)
 
-    if contours and area > 100:
-        largest_contour = max(contours, key=cv2.contourArea)
-        x, y, w, h = cv2.boundingRect(largest_contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+        if len(x_coords) > 50:
+            min_x = np.min(x_coords)
+            max_x = np.max(x_coords)
+            min_y = np.min(y_coords)
+            max_y = np.max(y_coords)
+
+            width = max_x - min_x + 1
+            height = max_y - min_y + 1
+
+            cv2.rectangle(frame, (min_x, min_y), (max_x, max_y), (0, 0, 0), 2)
+
     cv2.putText(frame, f"Area: {area:.0f} px", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     frame = cv2.resize(frame, (350, 300))
